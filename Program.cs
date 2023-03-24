b19355ec1bc8845bc4956d4ec4d0171baa74a233
PC4-IFLAB <PC4-IFLAB>
2023-03-24 09:06:56 +0700

    jurnal5
    update1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    public class penjumlahan
+    {
+        public static void Main(string[] args)
+        {
+            penjumlahan.jumlah3angka<int>(12,13,14);
+        }
+        static void jumlah3angka<T>(T angka1, T angka2, T angka3)
+        {
+            Console.WriteLine((dynamic)angka1+(dynamic)angka2+(dynamic)angka3);
+        }
+    }
+}
+ 
